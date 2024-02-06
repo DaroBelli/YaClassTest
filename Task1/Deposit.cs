d333615b3609c45145b7665420c92eae0f86ed04
Гера Мертинс <geramertins@mail.ru>
2024-02-06 10:50:05 +0300

    Создан класс для работы с данными по вкладу. Создан метод для расчёта процента по вкладу. Создан метод для расчёта денег на вкладе, после начисления процентов.

---
+++ Task1/Deposit.cs
@@ -0,0 +1,34 @@
+﻿namespace Task1
+{
+    public static class Deposit
+    {
+        /// <summary>
+        /// Возвращает процент по вкладу, взависимости от начального вклада.
+        /// </summary>
+        /// <param name="amountDeposit">Начальный вклад.</param>
+        /// <returns></returns>
+        private static int GetPercentage(decimal amountDeposit)
+        {
+            if (amountDeposit < 100)
+            {
+                return 5;
+            }
+            else if (amountDeposit >= 100 && amountDeposit <= 200)
+            {
+                return 7;
+            }
+
+            return 10;
+        }
+
+        /// <summary>
+        /// Возвращает сумму вклада после начисления процентов.
+        /// </summary>
+        /// <param name="amountDeposit">Начальный вклад.</param>
+        /// <returns></returns>
+        public static decimal GetAmountAfterDeposit(decimal amountDeposit)
+        {
+            return Math.Round(amountDeposit * (1 + GetPercentage(amountDeposit) / (decimal)100), 2, MidpointRounding.AwayFromZero);
+        }
+    }
+}
