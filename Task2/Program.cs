4a514dd994be515e6857e86f06cc7087eabd1db9
Гера Мертинс <geramertins@mail.ru>
2024-02-06 16:09:21 +0300

    Сделан вывод строки из массива в консоль.

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Task2;
+
+int[,,] mas = { { { 1, 2 },{ 3, 4 } },
+                { { 4, 5 }, { 6, 7 } },
+                { { 7, 8 }, { 9, 10 } },
+                { { 10, 11 }, { 12, 13 } } };
+
+Console.WriteLine(StringBuilderGenerate.GetStringBuilder(mas));
