80348c5ee51bb2f3dad8620bb6ad18daf51e4ab7
Гера Мертинс <geramertins@mail.ru>
2024-02-06 16:36:01 +0300

    Добавлен вывод ответа в консоль.

---
+++ Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿using Task3;
+
+int[] mas = [6, 7, 2, 3, 9, 4, 7, 1, 3, 6, 8, 3, 7, 8, 3];
+
+int position = Position.GetMinSumWithRight(mas);
+
+Console.WriteLine($"Сумма значений: {mas[position]} и {mas[position + 1]}, находящихся в массиве на позициях: {position} и {position + 1} - является минимальной " +
+    $"и равняется: {mas[position] + mas[position + 1]}");
