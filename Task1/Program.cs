850fd970b046735283ea0cb80d912637a7c71a48
Гера Мертинс <geramertins@mail.ru>
2024-02-06 10:50:54 +0300

    Сделано получение данных от пользователя и проверка их на правильность ввода, а так же дальнейшея их обратка и вывод итоговой суммы.

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Task1;
+
+while (true)
+{
+    Console.WriteLine("Введите сумму вклада:");
+    if (decimal.TryParse(Console.ReadLine(), out decimal amountDeposit))
+    {
+        if (amountDeposit > 0)
+        {
+            Console.WriteLine(Deposit.GetAmountAfterDeposit(amountDeposit));
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Сумма вклада не может быть отрицательной!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введите число!");
+    }
+    Console.WriteLine();
+}
