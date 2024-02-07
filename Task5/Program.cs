3beec3149afd5d2a9bcec74b2cf2f9cc32051b81
Гера Мертинс <geramertins@mail.ru>
2024-02-07 12:43:50 +0300

    Настроена работа с фигурами.

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,6 +1,20 @@
+using Task5.Services;
+using Task5.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddTransient<IFigureService, FigureService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/figuries", app.Services.GetService<IFigureService>().InfoFiguries).WithTags("Figure Endpoint");
 
 app.Run();
