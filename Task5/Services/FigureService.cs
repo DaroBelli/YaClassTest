b1b03a124d011625cef229989f5d7e198f0b6a6e
Гера Мертинс <geramertins@mail.ru>
2024-02-07 12:43:40 +0300

    Создан сервис для работы с фигурами.

---
+++ Task5/Services/FigureService.cs
@@ -0,0 +1,50 @@
+﻿using Task5.Models;
+using Task5.Services.Interfaces;
+using static Task5.Services.Interfaces.IFigureService;
+
+namespace Task5.Services
+{
+    public class FigureService : IFigureService
+    {
+        /// <summary>
+        /// Возвращает информацию по фигурам.
+        /// </summary>
+        /// <param name="count">Количество фигур.</param>
+        /// <param name="figureType">Тип фигуры.</param>
+        /// <returns></returns>
+        public string[] InfoFiguries(int count, FigureType figureType)
+        {
+            if(figureType == FigureType.Rectangle)
+            {
+                return GetInfoStrings<Rectangle>(count);
+            }
+            else if (figureType == FigureType.Circle)
+            {
+                return GetInfoStrings<Circle>(count);
+            }
+            else 
+            { 
+                return GetInfoStrings<Triangle>(count);
+            }
+        }
+
+        /// <summary>
+        /// Формирует массив строк с информацией по фигурам.
+        /// </summary>
+        /// <typeparam name="T">Фигура.</typeparam>
+        /// <param name="count">Количество фигур.</param>
+        /// <returns></returns>
+        private static string[] GetInfoStrings<T>(int count) where T : Figure, new()
+        {
+            string[] infoFiguries = new string[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                Figure figure = new T();
+                infoFiguries[i] = figure.InfoFigure();
+            }
+
+            return infoFiguries;
+        }
+    }
+}
---
+++ Task5/Services/Interfaces/IFigureService.cs
@@ -0,0 +1,16 @@
+﻿using Task5.Models;
+
+namespace Task5.Services.Interfaces
+{
+    public interface IFigureService
+    {
+        string[] InfoFiguries(int count, FigureType figureType);
+
+        enum FigureType
+        {
+            Rectangle,
+            Triangle,
+            Circle
+        }
+    }
+}
