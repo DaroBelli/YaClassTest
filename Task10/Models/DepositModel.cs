e1f9b4f05682dd7f33a046a4c5b2d30d8338bbdd
Гера Мертинс <geramertins@mail.ru>
2024-02-08 14:24:10 +0300

    Исправлена регулярка для проверки Deposit.

--- Task10/Models/DepositModel.cs
+++ Task10/Models/DepositModel.cs
@@ -6,7 +6,7 @@ namespace Task10.Models
     {
         [Display(Name = "Вклад:")]
         [Required(ErrorMessage = "Введённое значени не явлется вкладом!")]
-        [RegularExpression(@"^[0-9]+(\,[0-9]{1,2})$", ErrorMessage = "У вклада не может быть больше двух цифр после запятой!")]
+        [RegularExpression(@"^[0-9]+(\,[0-9]{1,2})?$", ErrorMessage = "У вклада не может быть больше двух цифр после запятой!")]
         public string Deposit {  get; set; } = string.Empty;
 
         public int MonthCount { get; set; }
