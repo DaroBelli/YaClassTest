40a32039e5e8ef3cd2e67ed9f30637461fa5e14d
Гера Мертинс <geramertins@mail.ru>
2024-02-08 13:56:01 +0300

    Создана модель для получения данных по вкладам.

---
+++ Task10/Models/DepositModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task10.Models
+{
+    public class DepositModel
+    {
+        [Display(Name = "Вклад:")]
+        [Required(ErrorMessage = "Введённое значени не явлется вкладом!")]
+        [RegularExpression(@"^[0-9]+(\,[0-9]{1,2})$", ErrorMessage = "У вклада не может быть больше двух цифр после запятой!")]
+        public string Deposit {  get; set; } = string.Empty;
+
+        public int MonthCount { get; set; }
+    }
+}
