0feebf34e17944aedbfff73124d43eb6a352cbfb
Гера Мертинс <geramertins@mail.ru>
2024-02-08 11:14:11 +0300

    Выполнено 8 задание.

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿string str = "тестовый ++ т0ек+с0т - выполнение";
+
+Console.WriteLine($"Количество плюсов и минусов = {str.Where(x => x == '+' || x == '-').Count()}");
+
+int count = str.Where( x => x == '0' ).Count();
+if (str.StartsWith( '0' ) )
+    count--;
+Console.WriteLine($"Количество элементов, перед которыми стоит 0 = {count}");
