6d2452995c54b1515638854d9f93dc75d0c1faa9
Гера Мертинс <geramertins@mail.ru>
2024-02-08 13:55:11 +0300

    Добавлен конфиг для работы с appsettings.json.

---
+++ Task10/Conrollers/ConfigJSON.cs
@@ -0,0 +1,19 @@
+﻿namespace Task10.Conrollers
+{
+    public class ConfigJSON
+    {
+        /// <summary>
+        /// Создаёт конфиг, для работы с appsettings.json.
+        /// </summary>
+        /// <returns></returns>
+        public static IConfigurationRoot GetConfig()
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+
+            return builder.Build();
+        }
+    }
+}
