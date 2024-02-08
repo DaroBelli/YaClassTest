14438c53c5bb98a2e87c18a91ba53f2ae3d0c9e6
Гера Мертинс <geramertins@mail.ru>
2024-02-08 14:25:04 +0300

    Настроено подключение к БД.

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Task10.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<YaClassTestContext>(options => options.UseSqlServer(connection));
 
 builder.Services.AddRazorPages();
 
