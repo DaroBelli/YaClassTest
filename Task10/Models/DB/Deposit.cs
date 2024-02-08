9497657bd9bee2c4d85c230ce219984c70c3bbeb
Гера Мертинс <geramertins@mail.ru>
2024-02-08 14:23:37 +0300

    Создана модель таблицы Deposit БД.

---
+++ Task10/Models/DB/Deposit.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task10.Models.DB;
+
+public partial class Deposit
+{
+    public decimal InitialDeposit { get; set; }
+
+    [Key]
+    public int Id { get; set; }
+
+    public int MonthCount { get; set; }
+}
