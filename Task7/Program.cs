beb4a0aaab0dacbb39a80a161be6c13671bf1ed7
Гера Мертинс <geramertins@mail.ru>
2024-02-08 10:54:04 +0300

    Создан проект для 7 задания.

---
+++ Task7/Program.cs
@@ -0,0 +1,8 @@
+﻿using Task7;
+
+Console.WriteLine("Введите вашу строку:");
+
+string str = Console.ReadLine() ?? string.Empty;
+
+Console.WriteLine(StringConvert.GetReplaceStringRegex(str));
+Console.WriteLine(StringConvert.GetReplaceStringNotRegex(str));
