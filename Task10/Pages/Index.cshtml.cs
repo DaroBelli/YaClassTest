7d113a4642822de6352fae3b947b49c447aef924
Гера Мертинс <geramertins@mail.ru>
2024-02-08 14:24:41 +0300

    Добавлена запись возможных вкладов в БД.

--- Task10/Pages/Index.cshtml.cs
+++ Task10/Pages/Index.cshtml.cs
@@ -1,21 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Task10.Conrollers;
+using Task10.Contexts;
 using Task10.Models;
+using Task10.Models.DB;
 
 namespace Task10.Pages
 {
     [IgnoreAntiforgeryToken]
-    public class IndexModel : PageModel
+    public class IndexModel(YaClassTestContext db) : PageModel
     {
+        private readonly YaClassTestContext context = db;
         public DepositModel? DepositModel { get; set; }
         public string Message { get; set; } = string.Empty;
 
-        public void OnPost(DepositModel depositModel) 
+        public async Task OnPost(DepositModel depositModel) 
         {
             if (ModelState.IsValid)
             {
-                Message = $"Конечная сумма составит: {Deposit.GetDepositFromProcent(decimal.Parse(depositModel.Deposit), depositModel.MonthCount)}";
+                decimal deposit = decimal.Parse(depositModel.Deposit);
+                Message = $"Конечная сумма составит: {DepositController.GetDepositFromProcent(deposit, depositModel.MonthCount)}";
+
+                await context.Deposits.AddAsync(new Deposit { InitialDeposit = deposit , MonthCount = depositModel.MonthCount});
+                await context.SaveChangesAsync();
             }
             else Message = "Значение введено не корректно!";
         }
