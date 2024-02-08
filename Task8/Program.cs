6235a5d2adae83b8907ab202eafcbf20b6fa111a
Гера Мертинс <geramertins@mail.ru>
2024-02-08 12:24:51 +0300

    Добавлен ввод строки пользователем. Доработки после изменения ТЗ.

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,8 +1,7 @@
-﻿string str = "тестовый ++ т0ек+с0т - выполнение";
+﻿Console.WriteLine("Введите вашу строку:");
+
+string str = Console.ReadLine() ?? string.Empty; 
 
 Console.WriteLine($"Количество плюсов и минусов = {str.Where(x => x == '+' || x == '-').Count()}");
 
-int count = str.Where( x => x == '0' ).Count();
-if (str.StartsWith( '0' ) )
-    count--;
-Console.WriteLine($"Количество элементов, перед которыми стоит 0 = {count}");
+Console.WriteLine($"Количество элементов, перед которыми стоит 0 = {str.Split("+0").Length + str.Split("-0").Length - 2}");
